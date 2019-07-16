af93ac0fe561131e0531856154159b7f91230ebd
flga <flga@users.noreply.github.com>
2019-07-17 00:19:13 +0100

    move version info into separate file

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 export FTB_VERSION=2.8.0
-export FTB_FREETYPE=freetype-$FT_VERSION
+export FTB_FREETYPE=freetype-$FTB_VERSION
 export FTB_ZLIB=zlib-1.2.11
 export FTB_LIBPNG=libpng-1.6.37
 export FTB_HARFBUZZ=harfbuzz-2.5.3
